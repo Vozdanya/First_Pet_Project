781aa4d0a27c372720a42ea95183e74237b8ec1a
Vozdanya <156104348+Vozdanya@users.noreply.github.com>
2024-07-21 05:37:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const wrapper = document.querySelector('.wrapper')
+const PopupWrapper = document.querySelector('.PopupWrapper')
+const loginLink = document.querySelector('.login-link')
+const registerLink = document.querySelector('.register-link')
+const btnPopup = document.querySelector('.btnLoginPopup')
+const btnClose = document.querySelector('.btnClose')
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    PopupWrapper.classList.add('active-popup');
+});
+
+btnClose.addEventListener('click', ()=> {
+    PopupWrapper.classList.remove('active-popup');
+});
+
+
